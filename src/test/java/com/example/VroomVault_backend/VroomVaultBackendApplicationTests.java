34b3bbab89cd08d4181bab90fde60b22160dab51
hrishabhpatle <hrishabh07patle@gmail.com>
2025-09-03 10:49:12 +0530

    Initial commit

---
+++ src/test/java/com/example/VroomVault_backend/VroomVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VroomVault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomVaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
